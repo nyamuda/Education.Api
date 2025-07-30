28192dc8fdb103348efb852253267b65d6b7ae0b
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-30 03:51:48 +0200

    created the `IExamBoardService` interface

---
+++ Services/Abstractions/ExamBoards/IExamBoardService.cs
@@ -0,0 +1,15 @@
+using Education.Api.Dtos.ExamBoards;
+using Education.Api.Models;
+
+namespace Education.Api.Services.Abstractions.ExamBoards;
+
+public interface IExamBoardService
+{
+    Task<ExamBoard> GetByIdAsync(int id);
+
+    Task<PageInfo<ExamBoardDto>> GetAsync(int page, int pageSize);
+
+    Task UpdateAsync(int id, UpdateExamBoardDto dto);
+
+    Task DeleteAsync(int id);
+}
