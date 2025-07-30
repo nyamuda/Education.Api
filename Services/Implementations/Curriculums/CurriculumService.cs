1acb860411d9e659d68c09130ac7358769f145d8
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-30 03:26:09 +0200

    Implemented the `GetAsync` CurriculumService method that retrieves a paginated list of curriculums

--- Services/Implementations/Curriculums/CurriculumService.cs
+++ Services/Implementations/Curriculums/CurriculumService.cs
@@ -1,5 +1,6 @@
 using Education.Api.Data;
 using Education.Api.Dtos.Curriculums;
+using Education.Api.Models;
 using Education.Api.Services.Abstractions.Curriculums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -25,5 +26,45 @@ public class CurriculumService(ApplicationDbContext context) : ICurriculumServic
                 )
                 .FirstOrDefaultAsync(c => c.Id.Equals(id))
             ?? throw new KeyNotFoundException($@"Curriculum with ID ""{id}"" does not exist.");
+    }
+
+    /// <summary>
+    /// Retrieves a paginated list of curriculums.
+    /// </summary>
+    /// <param name="page">The current page number.</param>
+    /// <param name="pageSize">The number of items to include per page.</param>
+    /// <returns>
+    /// A <see cref="PageInfo{CurriculumDto}"/> containing the list of curriculums for the specified page,
+    /// along with pagination metadata such as page number, page size, and whether more items are available.
+    /// </returns>
+    public async Task<PageInfo<CurriculumDto>> GetAsync(int page, int pageSize)
+    {
+        var query = _context.Curriculums.OrderByDescending(c => c.CreatedAt).AsQueryable();
+
+        List<CurriculumDto> items = await query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .Select(
+                c =>
+                    new CurriculumDto
+                    {
+                        Id = c.Id,
+                        Name = c.Name,
+                        CreatedAt = c.CreatedAt
+                    }
+            )
+            .ToListAsync();
+
+        //pagination info
+        int totalItems = await query.CountAsync();
+        bool hasMore = totalItems > page * pageSize;
+
+        return new PageInfo<CurriculumDto>
+        {
+            Page = page,
+            PageSize = pageSize,
+            HasMore = hasMore,
+            Items = items
+        };
     }
 }
