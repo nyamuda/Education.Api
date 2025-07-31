f3d92c1860c5c7f89382e5d6e05add8855176291
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-31 07:59:16 +0200

    created the `ISubTopicService` interface

---
+++ Services/Abstractions/Topics/ISubtopicService.cs
@@ -0,0 +1,17 @@
+using Education.Api.Dtos.Topics.Subtopics;
+using Education.Api.Models;
+
+namespace Education.Api.Services.Abstractions.Topics;
+
+public interface ISubTopicService
+{
+    Task<SubtopicDto> GetByIdAsync(int id);
+
+    Task<PageInfo<SubtopicDto>> GetAsync(int page, int pageSize);
+
+    Task<SubtopicDto> AddAsync(AddSubtopicDto dto);
+
+    Task UpdateAsync(int id, UpdateSubtopicDto dto);
+
+    Task DeleteAsync(int id);
+}
