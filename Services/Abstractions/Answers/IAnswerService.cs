75a483a6ca671532e0f61176aac6ccd075280aad
P.T Nyamuda <ptnrlab@gmail.com>
2025-07-31 18:13:02 +0200

    created the IAnswerService interface

--- Services/Abstractions/Answers/IAnswerService.cs
+++ Services/Abstractions/Answers/IAnswerService.cs
@@ -1,3 +1,17 @@
+using Education.Api.Dtos.Answers;
+using Education.Api.Models;
+
 namespace Education.Api.Services.Abstractions.Answers;
 
-public interface IAnswerService { }
+public interface IAnswerService
+{
+    Task<AnswerDto> GetByIdAsync(int id);
+
+    Task<PageInfo<AnswerDto>> GetAsync(int page, int pageSize);
+
+    Task<AnswerDto> AddAsync(int userId, AddAnswerDto dto);
+
+    Task UpdateAsync(int userId, int answerId, UpdateAnswerDto dto);
+
+    Task DeleteAsync(int userId, int answerId);
+}
