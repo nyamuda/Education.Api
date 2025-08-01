cc9fb0f27eb85263533681d142631d33f71d17aa
P.T Nyamuda <ptnrlab@gmail.com>
2025-08-01 18:22:19 +0200

    implemented methods for the `UpvoteService` that  add and remove upvotes from the specified answer

--- Services/Implementations/Upvotes/UpvoteService.cs
+++ Services/Implementations/Upvotes/UpvoteService.cs
@@ -124,7 +124,7 @@ public class UpvoteService(ApplicationDbContext context, ILogger<UpvoteService> 
         await _context.SaveChangesAsync();
 
         _logger.LogInformation(
-            "Successfully removed an upvote for question with ID {QuestionId}.",
+            "Successfully removed an upvote for a question with ID {QuestionId}.",
             questionId
         );
     }
@@ -134,14 +134,110 @@ public class UpvoteService(ApplicationDbContext context, ILogger<UpvoteService> 
     /// </summary>
     /// <param name="userId">The ID of the user upvoting the answer.</param>
     /// <param name="answerId">The ID of the answer to upvote.</param>
-    Task UpvoteAnswerAsync(int userId, int answerId);
+    /// <exception cref="KeyNotFoundException">
+    /// Thrown if the specified answer or user is not found.
+    /// </exception>
+    /// <exception cref="ConflictException">
+    /// Thrown if the specified user has already upvoted the same answer.
+    /// </exception>
+    public async Task UpvoteAnswerAsync(int userId, int answerId)
+    {
+        //check if there isn't already an existing upvote for the same answer by the same user
+        bool hasAlreadyUpvoted = await _context
+            .Upvotes
+            .Where(upv => upv.UserId.Equals(userId) && upv.AnswerId.Equals(answerId))
+            .AnyAsync();
+
+        if (hasAlreadyUpvoted)
+        {
+            _logger.LogWarning(
+                "Upvote skipped: User {UserId} has already upvoted answer {AnswerId}.",
+                userId,
+                answerId
+            );
+            throw new ConflictException(
+                $"Duplicate upvote: User with ID '{userId}' has already upvoted answer with ID '{answerId}'."
+            );
+        }
+
+        //check if the answer exists
+        var answer = await _context
+            .Answers
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id.Equals(answerId));
+
+        if (answer is null)
+        {
+            _logger.LogWarning(
+                "Upvote failed: Answer with ID {AnswerId} does not exist.",
+                answerId
+            );
+            throw new KeyNotFoundException($"Cannot upvote: No answer found with ID '{answerId}'.");
+        }
+
+        //check if the user casting the upvote exists
+        var user = await _context
+            .Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Id.Equals(userId));
+
+        if (user is null)
+        {
+            _logger.LogWarning(
+                "Answer upvote failed: User with ID {UserId} does not exist.",
+                userId
+            );
+
+            throw new KeyNotFoundException(
+                $"Cannot upvote answer: No user found with ID '{userId}'."
+            );
+        }
+
+        // upvote the answer and the save the upvote to the database
+        Upvote upvote = new() { UserId = userId, AnswerId = answerId };
+
+        await _context.Upvotes.AddAsync(upvote);
+
+        _logger.LogInformation("Successfully upvoted answer with ID {AnswerId}.", answerId);
+    }
 
     /// <summary>
     /// Removes an existing upvote from the specified answer by the given user.
     /// </summary>
     /// <param name="userId">The ID of the user removing their upvote.</param>
     /// <param name="answerId">The ID of the answer to remove the upvote from.</param>
-    Task RemoveAnswerUpvoteAsync(int userId, int answerId);
+    /// <exception cref="KeyNotFoundException">
+    /// Thrown if the specified upvote is not not found.
+    /// </exception>
+    public async Task RemoveAnswerUpvoteAsync(int userId, int answerId)
+    {
+        //check if the answer upvote exists
+        var answerUpvote = await _context
+            .Upvotes
+            .FirstOrDefaultAsync(upv => upv.UserId.Equals(userId) && upv.AnswerId.Equals(answerId));
+
+        if (answerUpvote is null)
+        {
+            _logger.LogWarning(
+                "Failed to remove upvote: Upvote for answer with ID {AnswerId} by user with ID {UserId} not found.",
+                answerId,
+                userId
+            );
+
+            throw new KeyNotFoundException(
+                $"Cannot remove upvote: Upvote for answer with ID '{answerId}' by user with ID '{userId}' does not exist."
+            );
+        }
+
+        //remove the upvote
+        _context.Upvotes.Remove(answerUpvote);
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation(
+            "Successfully removed an upvote for an answer with ID {AnswerId}.",
+            answerId
+        );
+    }
 
     /// <summary>
     /// Adds an upvote to the specified comment by the given user.
