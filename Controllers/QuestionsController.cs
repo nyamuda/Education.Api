b4b1de119df3761f037e2e3034ac382f2c2f75a9
P.T Nyamuda <ptnrlab@gmail.com>
2025-08-02 10:19:47 +0200

    added an endpoint that adds a new question to the database

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -1,4 +1,6 @@
+using Education.Api.Dtos.Questions;
 using Education.Api.Models;
+using Education.Api.Services.Abstractions.Auth;
 using Education.Api.Services.Abstractions.Questions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,9 +8,11 @@ namespace Education.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class QuestionsController(IQuestionService questionService) : ControllerBase
+public class QuestionsController(IQuestionService questionService, IJwtService jwtService)
+    : ControllerBase
 {
     private readonly IQuestionService _questionService = questionService;
+    private readonly IJwtService _jwtService = jwtService;
 
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(int id)
@@ -21,6 +25,54 @@ public class QuestionsController(IQuestionService questionService) : ControllerB
         catch (KeyNotFoundException ex)
         {
             return NotFound(ErrorResponse.Create(ex.Message));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ErrorResponse.Unexpected(ex.Message));
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get(int page = 1, int pageSize = 10)
+    {
+        try
+        {
+            var questions = await _questionService.GetAsync(page: page, pageSize: pageSize);
+            return Ok(questions);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ErrorResponse.Unexpected(ex.Message));
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(AddQuestionDto dto)
+    {
+        try
+        {
+            //retrieve the access token
+            string token = HttpContext
+                .Request
+                .Headers
+                .Authorization
+                .ToString()
+                .Replace("Bearer ", "");
+
+            //Validate the token and get the details of the user associated with it
+            (int userId, _, _) = _jwtService.ValidateTokenAndExtractUser(token);
+
+            QuestionDto question = await _questionService.AddAsync(userId, dto);
+
+            return CreatedAtAction(nameof(Get), new { id = question.Id }, question);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ErrorResponse.Create(ex.Message));
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(ErrorResponse.Create(ex.Message));
         }
         catch (Exception ex)
         {
