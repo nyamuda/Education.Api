f0a63a06254fbc20df239f9495fe9109f283f9b3
P.T Nyamuda <ptnrlab@gmail.com>
2025-08-02 17:43:54 +0200

    created a controller action method that flags a comment with a given ID

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -1,7 +1,12 @@
 using Education.Api.Dtos.Comments;
+using Education.Api.Dtos.Flags;
+using Education.Api.Dtos.Flags.Comments;
+using Education.Api.Exceptions;
 using Education.Api.Models;
 using Education.Api.Services.Abstractions.Auth;
 using Education.Api.Services.Abstractions.Comments;
+using Education.Api.Services.Abstractions.Flags;
+using Education.Api.Services.Abstractions.Upvotes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +14,17 @@ namespace Education.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CommentsController(ICommentService commentService, IJwtService jwtService)
-    : ControllerBase
+public class CommentsController(
+    ICommentService commentService,
+    IJwtService jwtService,
+    IUpvoteService upvoteService,
+    ICommentFlagService commentFlagService
+) : ControllerBase
 {
     private readonly ICommentService _commentService = commentService;
     private readonly IJwtService _jwtService = jwtService;
+    private readonly IUpvoteService _upvoteService = upvoteService;
+    private readonly ICommentFlagService _commentFlagService = commentFlagService;
 
     //Gets a comment with a given ID
     [HttpGet("{id}", Name = "GetCommentById")]
@@ -103,6 +114,118 @@ public class CommentsController(ICommentService commentService, IJwtService jwtS
         catch (UnauthorizedAccessException ex)
         {
             return Unauthorized(ErrorResponse.Create(ex.Message));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ErrorResponse.Unexpected(ex.Message));
+        }
+    }
+
+    //Upvotes a comment with a given ID
+    [HttpPost("{commentId}/upvotes")]
+    [Authorize]
+    public async Task<IActionResult> Upvote(int commentId)
+    {
+        try
+        {
+            //retrieve the access token
+            string token = HttpContext
+                .Request
+                .Headers
+                .Authorization
+                .ToString()
+                .Replace("Bearer ", "");
+
+            //Validate the token and get the details of the user associated with it
+            (int userId, _, _) = _jwtService.ValidateTokenAndExtractUser(token);
+
+            await _upvoteService.UpvoteCommentAsync(userId: userId, commentId: commentId);
+
+            return StatusCode(201);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ErrorResponse.Create(ex.Message));
+        }
+        catch (ConflictException ex)
+        {
+            return StatusCode(409, ErrorResponse.Create(ex.Message));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ErrorResponse.Unexpected(ex.Message));
+        }
+    }
+
+    //Removes an upvote for a comment with a given ID
+    [HttpDelete("{commentId}/upvotes")]
+    [Authorize]
+    public async Task<IActionResult> RemoveUpvote(int commentId)
+    {
+        try
+        {
+            //retrieve the access token
+            string token = HttpContext
+                .Request
+                .Headers
+                .Authorization
+                .ToString()
+                .Replace("Bearer ", "");
+
+            //Validate the token and get the details of the user associated with it
+            (int userId, _, _) = _jwtService.ValidateTokenAndExtractUser(token);
+
+            await _upvoteService.RemoveCommentUpvoteAsync(userId: userId, commentId: commentId);
+
+            return NoContent();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ErrorResponse.Create(ex.Message));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ErrorResponse.Unexpected(ex.Message));
+        }
+    }
+
+    //Flags a comment with a given ID
+    [HttpPost("{commentId}/flags")]
+    [Authorize]
+    public async Task<IActionResult> Flag(int commentId, AddCommentFlagDto dto)
+    {
+        try
+        {
+            //retrieve the access token
+            string token = HttpContext
+                .Request
+                .Headers
+                .Authorization
+                .ToString()
+                .Replace("Bearer ", "");
+
+            //Validate the token and get the details of the user associated with it
+            (int userId, _, _) = _jwtService.ValidateTokenAndExtractUser(token);
+
+            CommentFlagDto commentFlag = await _commentFlagService.AddAsync(
+                userId: userId,
+                commentId: commentId,
+                dto
+            );
+
+            return CreatedAtRoute(
+                routeName: "GetCommentFlagById",
+                routeValues: new { id = commentFlag.Id },
+                value: commentFlag
+            );
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ErrorResponse.Create(ex.Message));
+        }
+        catch (ConflictException ex)
+        {
+            return StatusCode(409, ErrorResponse.Create(ex.Message));
         }
         catch (Exception ex)
         {
