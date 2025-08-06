0de9f970ff1036ad31cd5787e1263cdda4aadb3d
P.T Nyamuda <ptnrlab@gmail.com>
2025-08-06 09:12:21 +0200

    implemented the service method that retrieves a paginated list of levels for a given exam board

--- Services/Implementations/Levels/LevelService.cs
+++ Services/Implementations/Levels/LevelService.cs
@@ -1,5 +1,6 @@
 using Education.Api.Data;
 using Education.Api.Dtos.Levels;
+using Education.Api.Models;
 using Education.Api.Services.Abstractions.Levels;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +18,7 @@ public class LevelService(ApplicationDbContext context, ILogger<LevelService> lo
      {
         return await _context.Levels.AsNoTracking().Select(l => new LevelDto
         {
-            Id = id,
+            Id =l.Id,
             Name = l.Name,
             ExamBoardId = l.ExamBoardId,
             CreatedAt = l.CreatedAt
@@ -25,7 +26,45 @@ public class LevelService(ApplicationDbContext context, ILogger<LevelService> lo
         throw new KeyNotFoundException($"Level not found: ID '{id}'");
      }
 
-    Task<PageInfo<LevelDto>> GetAsync(int examBoardId, int page, int pageSize);
+
+    /// <summary>
+    /// Retrieves a paginated list of levels for a given exam board.
+    /// </summary>
+    /// <param name="page">The current page number.</param>
+    /// <param name="pageSize">The number of items to include per page.</param>
+    /// <returns>
+    /// A <see cref="PageInfo{LevelDto}"/> containing the list of levels for the specified page,
+    /// along with pagination metadata such as page number, page size, and whether more items are available.
+    /// </returns>
+    public async Task<PageInfo<LevelDto>> GetAsync(int examBoardId, int page, int pageSize) 
+    {
+        var query = _context.Levels.Where(l => l.ExamBoardId.Equals(examBoardId)).AsQueryable();
+
+        List<LevelDto> items = await query
+        .Skip((page = 1) * pageSize)
+        .Take(pageSize)
+        .AsNoTracking()
+        .Select(l => new LevelDto
+        {
+            Id = l.Id,
+            Name = l.Name,
+            ExamBoardId = l.ExamBoardId,
+            CreatedAt = l.CreatedAt
+        }).ToListAsync();
+
+        //pagination metadata
+        int totalItems = await query.CountAsync();
+        bool hasMore = totalItems > page * pageSize;
+
+        return new PageInfo<LevelDto>
+        {
+            Items = items,
+            HasMore = hasMore,
+            Page = page,
+            PageSize = pageSize
+        };
+        
+    }
 
     Task<LevelDto> AddAsync(AddLevelDto dto);
 
