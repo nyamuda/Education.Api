0c434cff3ab009fb5b80dede51085a33d51f0868
P.T Nyamuda <ptnrlab@gmail.com>
2025-09-17 11:19:58 +0200

    implemented a service method that bookmarks a question for a user with a given ID.

--- Services/Implementations/Bookmarks/QuestionBookmarkService.cs
+++ Services/Implementations/Bookmarks/QuestionBookmarkService.cs
@@ -1,5 +1,7 @@
 using Education.Api.Data;
+using Education.Api.Models;
 using Education.Api.Services.Abstractions.Bookmarks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Education.Api.Services.Implementations.Bookmarks;
 
@@ -11,9 +13,48 @@ public class QuestionBookmarkService(
     public ApplicationDbContext _context = context;
     public ILogger<QuestionBookmarkService> _logger = logger;
 
-    public async Task AddAsync(int userId, int questionId) 
+    //Bookmarks a question for a user with a given ID.
+    public async Task AddAsync(int userId, int questionId)
     {
-        
+        //check if the user exists
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id.Equals(userId));
+        if (user == null)
+        {
+            _logger.LogWarning(
+                "Unable to bookmark question {questionId}: user {userId} not found.",
+                questionId,
+                userId
+            );
+            throw new KeyNotFoundException(
+                $"Failed to bookmark question. User with ID {userId} not found."
+            );
+        }
+        //check if the question exists
+        var question = await _context.Questions.FirstOrDefaultAsync(q => q.Id.Equals(questionId));
+        if (question == null)
+        {
+            _logger.LogWarning(
+                "Unable to bookmark question {questionId}: question {questionId} not found.",
+                questionId,
+                questionId
+            );
+            throw new KeyNotFoundException(
+                $"Failed to bookmark question. Question with ID {questionId} not found."
+            );
+        }
+
+        //create a bookmark for the question
+        QuestionBookmark bookmark = new() { QuestionId = questionId, UserId = userId };
+
+        _context.QuestionBookmarks.Add(bookmark);
+
+        await _context.SaveChangesAsync();
+
+        _logger.LogInformation(
+            "User {userId} successfully bookmarked question {questionId}.",
+            userId,
+            questionId
+        );
     }
 
     Task DeleteAsync(int userId, int questionId);
